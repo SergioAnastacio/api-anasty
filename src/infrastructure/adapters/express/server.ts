c657e114334463d89140aa1e3d353d09e32597ad
SergioAngulo <sergioangulorosales@gmail.com>
2024-12-11 03:49:22 -0600

    setup secrests.

--- src/infrastructure/adapters/express/server.ts
+++ src/infrastructure/adapters/express/server.ts
@@ -1,16 +1,18 @@
 // src/infrastructure/adapters/express/server.ts
-import express from "express";
-import { shortenUrlHandler } from "./controllers/UrlController"; // Corrige la extensión del archivo si es necesario
-import { redirectUrlHandler } from "./controllers/RedirectUrlController"; // Corrige la extensión del archivo si es necesario
-import { setupSwagger } from "./swaggerConfig.js";
+import express from 'express';
+import { shortenUrlHandler } from './controllers/UrlController.js';
+import { redirectUrlHandler } from './controllers/RedirectUrlController.js';
+import { setupSwagger } from './swaggerConfig.js';
 
 export const app = express();
 app.use(express.json());
 
 setupSwagger(app);
 
-app.post("/shorten", shortenUrlHandler);
-app.get("/:shortUrl", redirectUrlHandler);
+app.post('/shorten', shortenUrlHandler);
+app.get('/:shortUrl', redirectUrlHandler);
 
 const port = 3000;
-app.listen(port, () => {});
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
